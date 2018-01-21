d9e5aa8f0c6bf7458c0fe6d3b9aee8fc2a588371
MaximePAYRAUDEAU <mpayraudeau5@gmail.com>
2018-01-21 16:58:01 +0100

    Create API Definition

---
+++ todolistapi/api/models/todoListModel.js
@@ -0,0 +1,27 @@
+/**
+ * Created by X-Strike on 21/01/2018.
+ */
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var TaskSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Kindly enter the name of the task'
+    },
+    Created_date: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: [{
+            type: String,
+            enum: ['pending', 'ongoing', 'completed']
+        }],
+        default: ['pending']
+    }
+});
+
+module.exports = mongoose.model('Tasks', TaskSchema);
